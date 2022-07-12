7b239c62c0bc464fac43700f4f1ff2c9d94bcfdd
BrunoSabio <bruno.sabio@hotmail.com>
2022-07-12 17:53:08 -0300

    feat:construindo pagina de carrinho

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const controllersHomePage = require('../controllers/controllersPagina');
 var router = express.Router();
 const controllersPagina = require('../controllers/controllersPagina')
 
+const controllersCarrinho = require('../controllers/controllersCarrinho')
+
 router.get('/', controllersPagina.home)
 router.get('/cadastro', controllersPagina.cadastro)
 router.get('/produtos', controllersPagina.produtos)
+router.get('/carrinho', controllersCarrinho.carrinho)
 
 
 module.exports = router;
