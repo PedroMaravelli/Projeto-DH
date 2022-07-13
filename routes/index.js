8c7d68a7c3b13154bdd01c4e883e715f025eb496
brunoariel1 <brunoarieldealmeida@gmail.com>
2022-07-13 20:33:21 -0300

    update routes checkout

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
+const controllersHomePage = require('../controllers/controllersPagina');
 var router = express.Router();
 const controllersPagina = require('../controllers/controllersPagina')
 const controllersCheckout = require('../controllers/controllersCheckout')
+const controllersCarrinho = require('../controllers/controllersCarrinho')
 
 router.get('/', controllersPagina.home)
 router.get('/cadastro', controllersPagina.cadastro)
+router.get('/produtos', controllersPagina.produtos)
+router.get('/carrinho', controllersCarrinho.carrinho)
 router.get('/checkout', controllersCheckout.checkout)
 
 
-
-
 module.exports = router;
+
+
