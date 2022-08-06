e881eacec32e0e3678e36dff26986effb5fc3ffc
PedroMaravelli <phmaravelli123@gmail.com>
2022-08-06 16:04:16 -0300

    removendo arquivos

--- routes/adminCadastroProdutos.js
+++ routes/adminCadastroProdutos.js
@@ -2,11 +2,10 @@ var express = require('express');
 
 var router = express.Router();
 const controllersPagina = require('../controllers/controllersPagina')
-const validationsProducts = require('../midlawares/cadastroProdutosMiddlaware')
 const multerStorage = require('../midlawares/multerCadastroProdutos')
 const multer = require('multer');
 const multerDiskStorage = require('../midlawares/multerCadastroProdutos');
-const validations = require('../midlawares/cadastroMiddlaware')
+
 
 const upload = multer({storage:multerDiskStorage})
 
